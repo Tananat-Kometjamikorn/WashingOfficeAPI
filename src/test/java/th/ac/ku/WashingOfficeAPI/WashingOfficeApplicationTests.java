3c689973e879ff9c9906bc630adbb0575db33bac
Tananat-Kometjamikorn <tananat.k@ku.th>
2021-10-08 09:10:19 +0700

    Initailize API

---
+++ src/test/java/th/ac/ku/WashingOfficeAPI/WashingOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.WashingOfficeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
