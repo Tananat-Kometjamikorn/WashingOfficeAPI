6a03b7ecb6cba0277ce7a643b794f78b54423c80
Tananat-Kometjamikorn <tananat.k@ku.th>
2021-10-11 15:12:33 +0700

    start create api service

---
+++ src/main/java/th/ac/ku/service/BranchService.java
@@ -0,0 +1,34 @@
+package th.ac.ku.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import th.ac.ku.model.Branch;
+import th.ac.ku.repository.BranchRepository;
+
+import java.util.List;
+
+@Service
+public class BranchService {
+
+    @Autowired
+    private BranchRepository repository;
+
+    public List<Branch> getAll() {
+        return repository.findAll();
+    }
+
+    public Branch create(Branch branch){
+        repository.save(branch);
+        return branch;
+    }
+
+    public Branch update(String b_username, Branch requestBody){
+        Branch record = repository.findById(b_username).orElse(null);
+        if (record != null && requestBody.getB_password()!=null) {
+                record.setB_password(requestBody.getB_password());
+        }
+        else throw new IllegalArgumentException("Fill all blank lines");
+        repository.saveAndFlush(record);
+        return record;
+    }
+}
